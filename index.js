f72f419894f0fe2dd88b8909e39c17911d616aa7
MrFarhan <muhammadfarhan012345@gmail.com>
2021-04-04 01:07:47 +0500

    detected intent (send query in function and get bot's response in result) using dialogflow client library

--- index.js
+++ index.js
@@ -1,30 +1,73 @@
-const dialogflow = require('dialogflow');
-const sessionId = '123456';
-const languageCode = 'en';
+
 const credentials = require("./service_key.json")
-const projectId = credentials.project_id;// your dialogflow agent project Id here
-const eventName = "Welcome";// name of the event to be triggered
 
-const sessionClient = new dialogflow.SessionsClient({
-  keyFilename: "./service_key.json"// path to your service account key file here
-});
+function detectTextIntent(projectId, sessionId, queries, languageCode) {
 
-async function detectEventIntent(projectId, sessionId, eventName, languageCode) {
+    const dialogflow = require('@google-cloud/dialogflow');
 
-  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+    const sessionClient = new dialogflow.SessionsClient({
+        keyFilename: "./service_key.json"// path to your service account key file here
+    });
 
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      event: {
-        name: eventName,
-        languageCode: languageCode
-      }
-    },
-  };
-  const responses = await sessionClient.detectIntent(request);
+    async function detectIntent(
+        projectId,
+        sessionId,
+        query,
+        contexts,
+        languageCode
+    ) {
+        // The path to identify the agent that owns the created intent.
+        const sessionPath = sessionClient.projectAgentSessionPath(
+            projectId,
+            sessionId
+        );
 
-  console.log("fulfillment Text is : ", responses[0].queryResult.fulfillmentText);
+        // The text query request.
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    text: query,
+                    languageCode: languageCode,
+                },
+            },
+        };
+
+        if (contexts && contexts.length > 0) {
+            request.queryParams = {
+                contexts: contexts,
+            };
+        }
+
+        const responses = await sessionClient.detectIntent(request);
+        return responses[0];
+    }
+
+    async function executeQueries(projectId, sessionId, queries, languageCode) {
+        let context;
+        let intentResponse;
+        for (const query of queries) {
+            try {
+                console.log(`Sending Query: ${query}`);
+                intentResponse = await detectIntent(
+                    projectId,
+                    sessionId,
+                    query,
+                    context,
+                    languageCode
+                );
+                console.log('Detected intent');
+                console.log(
+                    `Fulfillment Text: ${intentResponse.queryResult.fulfillmentText}`
+                );
+                // Use the context from this response for next queries
+                context = intentResponse.queryResult.outputContexts;
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+    executeQueries(projectId, sessionId, queries, languageCode);
+    // [END dialogflow_detect_intent_text]
 }
-
-detectEventIntent(projectId, sessionId, eventName, languageCode)
+detectTextIntent(credentials.project_id, 1111, ["hi"], "en")
